b74ab53fd76fe9c4fb3923579a285223aa612015
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-11 20:10:57 +0100

    jk

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     size = len(argv)
     sum_ = 0
-    for i in range(1,size):
+    for i in range(1, size):
         sum_ += int(argv[i])
     print("{:d}".format(sum_))
