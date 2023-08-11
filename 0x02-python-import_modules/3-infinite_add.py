9c26548039d54c2642792d59eef8a7b346a60ab4
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-11 20:10:07 +0100

    jk

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    size = len(argv)
+    sum_ = 0
+    for i in range(1,size):
+        sum_ += int(argv[i])
+    print("{:d}".format(sum_))
