c53378235141ff8166057fbecf038a9b7a717bb6
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-11 19:46:54 +0100

    jk

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    size = len(argv)
+    if size == 1:
+        str_ = "arguments."
+    elif size == 2:
+        str_ = "argument:"
+    elif size > 2:
+        str_ = "arguments:"
+
+    print("{:d} {}".format(size - 1, str_), end="\n")
+    if size > 0:
+        for i in range(1,size):
+            print("{:d}: {}".format(i, argv[i]), end="\n")
