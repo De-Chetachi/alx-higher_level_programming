37c762f51687bf47055772d2611395ba11394f59
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-13 20:23:47 +0100

    go

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        a = len(sentence)
-        if (a > 0):
-            b = sentence[0]
+    a = len(sentence)
+    if sentence and a > 0:
+        b = sentence[0]
     else:
         b = "None"
     c = a, b
