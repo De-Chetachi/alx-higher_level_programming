cf7ae5962c3c2c9d7899ee3dc50c510e9597db06
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-13 17:15:40 +0100

    go

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def no_c(my_string):
+    new_str = ""
+    if my_string:
+        for c in my_string:
+            if ord('c') != ord(c) and ord('C') != ord(c):
+                new_str += c
+    return (new_str)
+
