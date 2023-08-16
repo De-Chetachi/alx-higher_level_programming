b1ff30c746f5fb7868304e50f1c68fb1fe6a526c
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-17 00:04:40 +0100

    created a new list of keys

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+a function that deletes keys with a specific value in a dictionary
+If the value doesn’t exist, the dictionary won’t change
+All keys having the searched value have to be deleted
+'''
+
+
+def complex_delete(a_dictionary, value):
+    keys = []
+    for key, val in  a_dictionary.items():
+        if val == value:
+            keys.append(key)
+    for i in keys:
+        del a_dictionary[i]
+
+    return (a_dictionary)
