ddb0b5403af6b88bda95629e009cd5ff681afe91
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-19 01:41:30 +0100

    yay

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -12,15 +12,15 @@ void print_python_bytes(PyObject *p)
 	PyBytesObject *bytes;
 	long int i, size, size_;
 
+	printf("[.] bytes object info\n");
 	if (PyBytes_CheckExact(p))
 	{
 		bytes = (PyBytesObject *)p;
 		size = bytes->ob_base.ob_size;
 		size_ = size + 1;
 
-		printf("[.] bytes object info\n");
-		printf("size: %ld\n", bytes->ob_base.ob_size);
-		printf("trying string: ");
+		printf("  size: %ld\n", bytes->ob_base.ob_size);
+		printf("  trying string: ");
 		for (i = 0; i < size; i++)
 			printf("%c", bytes->ob_sval[i]);
 		printf("\n");
@@ -28,7 +28,7 @@ void print_python_bytes(PyObject *p)
 		{
 			size_ = 10;
 		}
-		printf("first %ld bytes:", size_);
+		printf("  first %ld bytes:", size_);
 
 		for (i = 0; i < size_; i++)
 			printf(" %02x", bytes->ob_sval[i] & 0xff);
@@ -53,7 +53,7 @@ void print_python_list(PyObject *p)
 	list_ = (PyListObject *)p;
 	size = list_->ob_base.ob_size;
 
-	printf("[*] Python list info");
+	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", list_->ob_base.ob_size);
 	printf("[*] Allocated = %ld\n", list_->allocated);
 	for (i = 0; i < size; i++)
