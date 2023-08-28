fa4c491756344ada7d24b57912527594aea9f7db
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-28 19:45:35 +0100

    ok

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     res = 0
     try:
-        for i in range(0,x):
+        for i in range(0, x):
             print("{}".format(my_list[i]), end="")
             res += 1
-    except:
+    except IndexError:
         pass
     print()
     return (res)
