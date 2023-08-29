f3294419fb59cd35ffa0f84f9c690179493207fb
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-29 20:01:47 +0100

    created a square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+'''this module contains the creation of a class Square'''
+
+
+class Square:
+    '''this is a square class, which defines a private
+    instance attribute size'''
+    def __init__(self, size):
+        ''''this method defines a  private instance
+        attribute size'''
+        self.__size = size
