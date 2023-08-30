b3d33799141474e4b45792a5bc5e76bf8f863c72
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-30 23:20:46 +0100

    yay

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+''''this module defines a magic class'''
+
+
+class MagicClass:
+    '''this class defines a magic class:)'''
+    def __init__(self, radius=0):
+        '''the initializer'''
+        self.__radius = 0
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area():
+        '''defines the radius of the class'''
+        return((self.__radius ** 2) * math.pi)
+
+    def circumference():
+        '''defines the circumference of the class'''
+        return((2 * math.pi) * self.__radius)
