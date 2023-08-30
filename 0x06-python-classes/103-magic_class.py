c0fd1e04cf8bccc680535bcc7decc0f99ddc4fa4
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-30 23:42:39 +0100

    yay

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,8 +10,8 @@ class MagicClass:
         '''the initializer'''
         self.__radius = 0
         if type(radius) is not int:
-            #if type(radius) is not float:
-            raise TypeError("radius must be a number")
+            if type(radius) is not float:
+                raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area():
