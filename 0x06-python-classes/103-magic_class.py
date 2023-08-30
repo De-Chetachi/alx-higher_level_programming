ef96aab99d490bf60a4b31826ee6c491c2dcc210
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-08-30 23:59:55 +0100

    yay

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-''''this module defines a magic class'''
+''''this module defines
+a magic class'''
 
 
 import math
-'''defines math formulas'''
+'''defines math
+formulas'''
 
 
 class MagicClass:
-    '''this class defines a magic class:)'''
+    '''this class defines
+    a magic class:)'''
 
     def __init__(self, radius=0):
-        '''the initializer'''
+        '''the
+        initializer'''
 
         self.__radius = 0
         if type(radius) is not int:
@@ -19,11 +23,13 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        '''defines the radius of the class'''
+        '''defines the radius
+        of the class'''
 
         return((self.__radius ** 2) * math.pi)
 
     def circumference(self):
-        '''defines the circumference of the class'''
+        '''defines the circumference
+        of the class'''
 
         return((2 * math.pi) * self.__radius)
