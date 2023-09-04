f47b48ec19bee09b553d2213cce063ccd7081db2
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-04 20:04:47 +0100

    :(

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''this module contains a rectangle class'''
+
+
+class Rectangle:
+    '''class Rectangle that defines a rectangle by'''
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        '''gets the width property'''
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        '''sets width property'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''gets the height property'''
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        '''sets height property'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
