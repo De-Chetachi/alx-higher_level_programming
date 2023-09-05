0c0ebf7088834853c2fef794584f0e98686697d6
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-05 22:06:53 +0100

    _

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(list_=[]):
+    list_.append("BestSchool")
+    return (", ".join(list_))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''class module'''
+
+class LockedClass:
+    '''defines a locked class'''
+    __slots__ = ["first_name"]
