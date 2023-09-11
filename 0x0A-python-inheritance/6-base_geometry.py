fcf092bced0cfb5712f00966a1b5dc7da8a33de0
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-11 20:48:48 +0100

    o

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''base geometry module'''
+
+
+class BaseGeometry:
+    '''an empty class BaseGeometry'''
+
+    def area(self):
+        '''raises an Exception'''
+        raise Exception("area() is not implemented")
