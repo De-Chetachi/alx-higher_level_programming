3c939487cbdb9c6e25427029af8745b6c106879f
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-11 21:46:10 +0100

    o

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this module contains
+a class Rectangle that inherits from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''a class Rectangle that inherits
+    from BaseGeometry'''
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return(self.__height * self.__width)
+
+    def __str__(self):
+        '''defines a private instsance str method'''
+        rec = "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        return (rec)
