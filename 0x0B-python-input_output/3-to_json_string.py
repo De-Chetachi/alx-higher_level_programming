3f7c0578c66508452a2b2aee219eddd821b4126e
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-12 21:01:01 +0100

    json str rep

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''json module'''
+
+
+from json import dumps
+def to_json_string(my_obj):
+    '''function that returns the JSON representation of an object (string)'''
+    return (dumps(my_obj))
