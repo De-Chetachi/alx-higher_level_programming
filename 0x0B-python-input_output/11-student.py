f8d79b7662db934646ee8e8201789bef19fb920d
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-09-13 22:41:50 +0100

    hk

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''json module'''
+
+
+class Student:
+    ''' class Student that defines a student by:'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''a oublic method  that retrieves a dictionary
+        representation of a Student instance'''
+        if type(attrs) is not list:
+            return (self.__dict__)
+
+        new = True
+        for element in attrs:
+            if type(element) != str:
+                new = False
+        if new:
+            dict_ = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    dict_[attr] = self.__dict__[attr]
+            return (dict_)
+        else:
+            return (self.__dict__)
+
+    def reload_from_json(self, json):
+        '''replaces all attributes of the Student instance:'''
+        for key in json:
+            self.key = json[key];
