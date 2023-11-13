f752fcd18351607ad2a22e82f5a3f926ac045fed
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-13 23:16:17 +0100

    amazing

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Write a script that prints “JavaScript is amazing” */
+const string = [];
+string[0] = 'C is fun';
+string[1] = 'Python is cool';
+string[2] = 'JavaScript is amazing';
+for (let idx in string) {
+  console.log(string[idx]);
+}
