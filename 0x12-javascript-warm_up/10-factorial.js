7b3ead32c89e036cea4726e83400c7d138728d52
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-13 23:52:49 +0100

    amazing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@
 const argv = (require('process')).argv;
 const num = Number(argv[2]);
 
-function fact(a) {
+function fact (a) {
   if (isNaN(num)) return 1;
   if (a === 1) return 1;
   return (a * fact(a - 1));
