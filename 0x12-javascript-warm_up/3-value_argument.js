0294db6403323f8537a3a84a46836a5ecccd1179
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-13 22:34:49 +0100

    amazing

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Write a script that prints “JavaScript is amazing” */
+
+const argv = (require('node:process')).argv;
+
+if (argv[2]) {
+  console.log(argv[2]);
+}
+else {
+  console.log('No argument');
+}
