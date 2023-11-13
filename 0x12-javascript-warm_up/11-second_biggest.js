8f0f63a0041e12e1cdb134da20e2216dc2e57c45
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-14 00:19:23 +0100

    amazing

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* Write a script that prints “JavaScript is amazing” */
+
+const argv = (require('process')).argv;
+const len = argv.length;
+const num = Number(argv[2]);
+
+if (isNaN(num) || len === 3) {
+  console.log('0');
+} else {
+  const argvNew = argv.slice(3);
+  let max = num;
+  for (let idx in argvNew) {
+     const numa = Number(argvNew[idx]);
+    if (numa > num) max = numa;
+  }
+  console.log(max);
+}
