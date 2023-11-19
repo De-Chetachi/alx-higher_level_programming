4958c89932e0d7f80b7fbc6d55335cfc6e0c8946
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-19 20:54:35 +0100

    latin

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,7 @@ def main():
         cur = connect.cursor()
         query = "SELECT name FROM cities "
         query += "WHERE state_id = (SELECT id FROM states "
-        query += "WHERE name = %s)"
+        query += "WHERE name = %s) ORDER BY cities.id"
         cur.execute(query, (match, ))
         result = cur.fetchall()
         length = len(result)
