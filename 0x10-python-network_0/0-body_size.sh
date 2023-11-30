3b4f476c74adaf14b3d9ca3d673e65360a106664
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-30 21:30:20 +0100

    ok

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -is $1 | grep Content-Length: | tr -d 'Content-Length: '
