012b0967019b1d0d798125b935456aed98fd3996
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-01-31 22:30:29 +0100

    semitandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,14 +3,13 @@
 
 const fs = require('fs').promises;
 
-async function readFile(filePath) {
-	try {
-		const content = await fs.readFile(filePath, "utf-8");
-		console.log(content);
-	}
-	catch (err) {
-		console.log(err);
-	}
+async function readFile (filePath) {
+  try {
+    const content = await fs.readFile(filePath, 'utf-8');
+    console.log(content);
+  } catch (err) {
+    console.log(err);
+  }
 }
-file = process.argv[2];
+const file = process.argv[2];
 readFile(file);
